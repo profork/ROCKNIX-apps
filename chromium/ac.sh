2efe35429078cd09fbf1fa3602d45b10327544d2
Cliffy <cliffypro@proton.me>
2025-08-06 10:25:06 +0300

    +

---
+++ chromium/ac.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Downloads and runs the arch-kodi.sh installer from GitHub
+
+cd ~/
+wget https://github.com/profork/ROCKNIX-apps/raw/main/chromium/arch-chromium.sh
+chmod +x arch-chromium.sh
+clear
+echo "Start install script by typing ./arch-chromium.sh"
