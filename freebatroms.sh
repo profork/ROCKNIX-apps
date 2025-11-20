54541316119be6e42ab8bcfa589f467e807dd863
Cliffy <cliffypro@proton.me>
2025-11-20 15:24:27 -0500

    +

--- freebatroms.sh
+++ freebatroms.sh
@@ -29,7 +29,7 @@ download() {
     return 0
   fi
   mkdir -p "$(dirname "$out")"
-  wget -q --show-progress -O "$out".partial "$url"
+  wget -q --no-check-certificate --retry-connrefused -O "$out".partial "$url"
   mv -f "$out".partial "$out"
   log "✔ Saved: $out"
 }
